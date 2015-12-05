bf51019ceeee1d7033eb8ae5862e524fdd85676b
Cason Barnhill <casonbarnhill@yahoo.com>
2015-12-04 23:15:04 -0600

    adding classes

---
+++ ComicCreator/ComicCreator.API/Models/Project.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicCreator.API.Models
+{
+    public class Project
+    {
+        public int ProjectId { get; set; }
+        public string Title { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+        public List<Tile> tiles {get; set;}
+    }
+    public class Tile
+    {
+        public int TileId { get; set; }
+        public float PositionX = 0.0f;
+        public float PositionY = 0.0f;
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+        public Project Project { get; set; }
+        public int OrderNumber { get; set; }
+    }
+}
