31624f8af63284fdfe8e074a35e6e9784bd527e0
Cason Barnhill <casonbarnhill@yahoo.com>
2015-12-04 22:46:47 -0600

    enabled cors.

--- ComicCreator/ComicCreator.API/App_Start/WebApiConfig.cs
+++ ComicCreator/ComicCreator.API/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace ComicCreator.API
 {
@@ -11,6 +12,8 @@ namespace ComicCreator.API
         {
             // Web API configuration and services
 
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
             // Web API routes
             config.MapHttpAttributeRoutes();
 
