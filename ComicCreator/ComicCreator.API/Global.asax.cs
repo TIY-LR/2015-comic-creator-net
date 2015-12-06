5898984a9863661117dcc91183bf48cb029a8344
Cason Barnhill <casonbarnhill@yahoo.com>
2015-12-05 18:40:12 -0600

    build fix

--- ComicCreator/ComicCreator.API/Global.asax.cs
+++ ComicCreator/ComicCreator.API/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ComicCreator.API.Migrations;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
